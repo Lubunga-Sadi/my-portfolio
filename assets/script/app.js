ef31a12bb256fa2baa61b069d41b5a38d73c576b
Lubunga <innocentsadi6@gmail.com>
2024-03-08 19:41:08 -0600

    first commit

---
+++ assets/script/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// This app requires a server to handle import statements
+// ans CORS issues
+import * as utils from './utils.js';
+
+/*------------------------------------------*/
+/*   Organizer                              */
+/*------------------------------------------*/
+
+utils.print('Welcome');
