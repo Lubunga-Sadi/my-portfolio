bbfcb9c5a182f76987d83d2ef41671726f9f0b85
Lubunga <innocentsadi6@gmail.com>
2024-03-11 09:06:54 -0600

    fade function fix

--- assets/script/app.js
+++ assets/script/app.js
@@ -2,10 +2,31 @@
 
 import * as utils from './utils.js';
 
-const fadeIn = utils.select('.fade-in');
-
 
 window.addEventListener('load', () => {
-  fadeIn.classList.add('hidden');
+  const sr = ScrollReveal();
+
+  const revealOptions = {
+    reset: true 
+  };
+  
+  sr.reveal('.fade-one', revealOptions);
+  sr.reveal('.fade-two', revealOptions);
+  sr.reveal('.contact', revealOptions);
 });
+
+/*
+const positionChange = utils.select('.shift')
+
+window.addEventListener('load', () => {
+  positionChange.style.position = 'relative';
+  positionChange.style.top = '200px'; 
+
+  setTimeout(() => {
+    positionChange.classList.add('transition');
+    positionChange.style.top = '0px'; 
+  }, 100);
+})*/
+
+
 
