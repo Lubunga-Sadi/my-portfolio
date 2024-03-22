9e587a92fad355df13ca8856a23878c4d1cde686
Lubunga <innocentsadi6@gmail.com>
2024-03-21 21:31:55 -0500

    projects section video hover

--- assets/script/app.js
+++ assets/script/app.js
@@ -12,6 +12,29 @@ window.addEventListener('load', () => {
   sr.reveal('.contact', revealOptions);
 });
 
+const aboutMeSection = document.querySelector('.about-me');
+const contactSection = document.querySelector('.contact');
+const aboutMeLink = document.querySelector('.about-sec');
+const contactLink = document.querySelector('.con-sec');
 
+const body = document.querySelector('body');
 
+aboutMeLink.addEventListener('click', () => {
+  aboutMeSection.scrollIntoView({ behavior: 'smooth' });
+})
 
+contactLink.addEventListener('click', () => {
+  contactSection.scrollIntoView({ behavior: 'smooth' });
+})
+
+const boxHover = document.querySelector('.projects a');
+const video = document.querySelector('.projects a .link-pic');
+
+boxHover.addEventListener('mouseenter', () => {
+  video.play();
+});
+
+boxHover.addEventListener('mouseleave', () => {
+  video.pause();
+  video.currentTime = 0;
+});
